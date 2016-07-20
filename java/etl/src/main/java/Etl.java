1d5bcd65f18e5b20937d43497af6e3e8e655bb1e
jbradley <jbradley@hubspot.com>
2016-07-20 17:24:48 -0400

    updated etl

--- java/etl/src/main/java/Etl.java
+++ java/etl/src/main/java/Etl.java
@@ -1,18 +1,17 @@
-import java.util.EnumMap;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
 
 public class Etl {
 
-   public static EnumMap<ScrabbleTile, Integer> scrabbleTileValueMap = new EnumMap<ScrabbleTile, Integer>(ScrabbleTile.class);
-
-   static {
-      scrabbleTileValueMap.put(ScrabbleTile.A, 1);
-      scrabbleTileValueMap.put(ScrabbleTile.B, 3);
-      scrabbleTileValueMap.put(ScrabbleTile.C, 3);
-   }
-
    public Map<String, Integer> transform(Map<Integer, List<String>> old) {
-      return null;
+      final Map<String, Integer> tileScoreMap = new HashMap<String, Integer>();
+      for (Entry<Integer, List<String>> entry : old.entrySet()) {
+         for (String s : entry.getValue()) {
+            tileScoreMap.put(s.toLowerCase(), entry.getKey());
+         }
+      }
+      return tileScoreMap;
    }
 }
