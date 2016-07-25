ec8c5bf187961acc860acb5410a171e8205ddc05
jaebradley <jae.b.bradley@gmail.com>
2016-07-24 23:20:01 -0400

    hamming it up

--- java/hamming/src/main/java/Hamming.java
+++ java/hamming/src/main/java/Hamming.java
@@ -1,2 +1,16 @@
 public class Hamming {
+
+  public static int compute(final String dnaStrand1, final String dnaStrand2) {
+    if (dnaStrand1.length() != dnaStrand2.length()) {
+      throw new IllegalArgumentException();
+    }
+
+    int count = 0;
+    for (int i = 0; i < dnaStrand1.length(); i++) {
+      if (dnaStrand1.charAt(i) != dnaStrand2.charAt(i)) {
+        count++;
+      }
+    }
+    return count;
+  }
 }
